02e44e4946ffdb6c28312ab961c57aee1c3366e5
unknown <stp40@case.edu>
2022-05-22 19:15:35 -0400

    project setup

---
+++ Desktop/Front End Projects/portfolio-website/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navigation from './Navigation.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <React.Fragment>
+      <Navigation />
+
+    </React.Fragment>
+  );
+}
+
+export default App;
